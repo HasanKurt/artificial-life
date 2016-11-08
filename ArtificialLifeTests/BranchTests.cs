17dd1cf5d714b0144c7ecc2a5b37e754f1ac0669
PROD\hkurt <hasan.kurt@alten.nl>
2016-11-08 10:50:55 +0100

    commit from msvs

---
+++ ArtificialLifeTests/BranchTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ArtificialLife;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtificialLife.Tests
+{
+    [TestClass()]
+    public class BranchTests
+    {
+        [TestMethod()]
+        public void BranchTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GrowTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DrawTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
